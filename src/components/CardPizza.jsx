16c689b55febb0e08d1bc52df38de016a69943ae
Jose Carmona <josshue85@gmail.com>
2024-12-15 00:30:26 -0300

    componente home listo (header y cardpizza: ok)

--- src/components/CardPizza.jsx
+++ src/components/CardPizza.jsx
@@ -1,7 +1,39 @@
-const CardPizza = () => {
+/************************************************************** */
+/* ************************ COMPONENTE ************************ */
+/************************************************************** */
+const CardPizza = ({name, price, ingredients, img}) => {
+    /* INGREDIENTES, con el método join convierto un arreglo en un string*/
+    // const ingredientes = ingredients.map((ingredient) => ingredient)
+    const ingredientes = ingredients.join(", ")
+    // console.log(ingredientes)
+
     return (
-        <article>
-            <h2>CardPizza</h2>
+        /* ****** CARD ****** */
+        <article className = "card">
+            {/* CARD IMAGE */}
+            <img src = {img} alt = {"Pizza " + name} className="card-image" />
+            {/* CARD BODY */}
+            <div className="card-body">
+                {/* NOMBRE */}
+                <h3 className="pizza-name">{"Pizza " + name}</h3>
+                {/* INGREDIENTES */}
+                <p className="ingredientes">
+                    Ingredientes:
+                    <br />
+                    {/* UTF-8 EMOJI PIZZA: &#x1F355; */}
+                    <span className="span-ingredientes">&#127829; {ingredientes.toString()}</span>
+                </p>
+                {/* PRECIO & BOTONES DE VER MÁS Y AÑADIR AL CARRO DE COMPRAS */}
+                <div className="precio-botones">
+                    <h2>Precio: ${price.toLocaleString('es-CL')}</h2>
+                    <div className="buttons">
+                        {/* VER MÁS */}
+                        <button className="button-ver-mas">Ver más &#128064;</button>
+                        {/* AÑADIR AL CARRO DE COMPRAS */}
+                        <button className="button-añadir-carro">Añadir &#128722;</button>
+                    </div>
+                </div>
+            </div>
         </article>
     )
 }
